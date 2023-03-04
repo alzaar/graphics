f3ca04bf32900c17d771682ca3811373828a3d42
Ali Zahir <Ali>
2023-03-04 05:49:40 -0500

    add basic webpack template

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+// const webpack = require('webpack'); //to access built-in plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  mode: 'development',
+  // Sample Loader
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+}
